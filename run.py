1d0359c63985ebc2653e6a3c6b65bac15c112eb9
Deborah Galasso <galassodeborah@outlook.com>
2023-06-27 17:22:48 +0000

    Add scheleton of README

--- run.py
+++ run.py
@@ -9,5 +9,5 @@ SCOPE = [
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.autorize(SCOPED_CREDS)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('expenses_tracker')
